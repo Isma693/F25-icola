a6474e3325e777d4ccecabbc46fd2b298a3e64f3
Isma693 <ismael.lehmann@he-arc.ch>
2025-11-11 19:55:17 +0100

    Ajoute l’écran admin "Mes Bières" avec utilisation du widget beer_menu et bouton d'ajout d'une nouvelle bière

---
+++ brewmatch/lib/screens/admin/myBeers_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../core/widgets/beer_menu.dart';
+import '../client/client_home_screen.dart';
+import 'beer_form_screen.dart';
+
+/// Écran temporaire qui encapsule le catalogue de bières pour l’espace admin.
+/// TODO: enrichir avec filtres avancés et actions CRUD.
+class MyBeersScreen extends StatelessWidget {
+  static const routeSegment = 'beers';
+
+  const MyBeersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mes Bières'),
+      ),
+      body: BeerListScreen(
+        filters: null,
+        onBeerTap: (beer) => context.push(
+          '${ClientHomeScreen.routePath}/beers/${beer.id}',
+          extra: beer,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.push(BeerFormScreen.routePath),
+        tooltip: 'Ajouter une bière',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
